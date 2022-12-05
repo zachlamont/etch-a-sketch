de7074227812f3ad2036ec357cf3c5153719eaf9
zachlamont <zachlamont1@gmail.com>
2022-12-06 00:18:38 +1000

    Add color picker

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
 let gridSize = 16;
 
 const container = document.querySelector("#grid-container"); // assign container div to a variable
-const pixels = document.querySelector(".pixels");
+const pixels = document.querySelectorAll(".pixels");
 const mouseInfo = document.getElementById("mouse-info");
+const rangeInput = document.getElementById("resolution");
+const colorPicker = document.getElementById("color-picker");
+
+function LightenDarkenColor(col, amt) {
+  col = parseInt(col, 16);
+  return (
+    ((col & 0x0000ff) + amt) |
+    ((((col >> 8) & 0x00ff) + amt) << 8) |
+    (((col >> 16) + amt) << 16)
+  ).toString(16);
+}
+// TEST
+//console.log( LightenDarkenColor("3F6D2A",40) );
 
 function drawGrid(gridSize) {
   for (i = 0; i < gridSize ** 2; i++) {
@@ -21,13 +34,17 @@ function drawGrid(gridSize) {
     pixel.style.width = (1 / gridSize) * 100 + "%";
     pixel.style.height = (1 / gridSize) * 100 + "%";
 
-   let pixelLightness = 80;
+    let pixelLightness = 1;
 
     pixel.addEventListener("mouseover", (event) => {
       // highlight the mouseover target
+
       if (mouseDown == 1) {
-      event.target.style.backgroundColor = "hsl(240,100%,"+pixelLightness+"%)";
-      pixelLightness = pixelLightness - 10;
+        var pickedColor = document.getElementById("color-picker").value;
+
+        event.target.style.backgroundColor = pickedColor;
+
+        //pixelLightness = pixelLightness - 0.1;
       }
     });
 
@@ -37,17 +54,24 @@ function drawGrid(gridSize) {
 
 drawGrid(32);
 
+rangeInput.addEventListener("mouseup", (event) => {
+  const pixels = document.querySelectorAll(".pixels");
+
+  pixels.forEach((pixel) => {
+    pixel.remove();
+  });
+  drawGrid(event.target.value);
+});
+
 let mouseDown = 0;
 document.body.onmousedown = function () {
   mouseDown = 1;
-  mouseInfo.textContent = 'Mouse is Down!';
+  mouseInfo.textContent = "Mouse is Down!";
 };
 document.body.onmouseup = function () {
   mouseDown = 0;
-  mouseInfo.textContent = 'Mouse is Up!';
+  mouseInfo.textContent = "Mouse is Up!";
 };
-
-
 
 /*
 function draw() {
