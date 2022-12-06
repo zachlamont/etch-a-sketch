5c8bd1164d5640bb61d0e133699ca63ad7e25b45
zachlamont <zachlamont1@gmail.com>
2022-12-06 19:05:41 +1000

    Remove unused code

--- javascript.js
+++ javascript.js
@@ -1,96 +1,49 @@
-let gridSize = 16;
-
-const container = document.querySelector("#grid-container"); // assign container div to a variable
+const container = document.querySelector("#grid-container");
 const pixels = document.querySelectorAll(".pixels");
 const mouseInfo = document.getElementById("mouse-info");
 const rangeInput = document.getElementById("resolution");
 const colorPicker = document.getElementById("color-picker");
 
-function LightenDarkenColor(col, amt) {
-  col = parseInt(col, 16);
-  return (
-    ((col & 0x0000ff) + amt) |
-    ((((col >> 8) & 0x00ff) + amt) << 8) |
-    (((col >> 16) + amt) << 16)
-  ).toString(16);
-}
-// TEST
-//console.log( LightenDarkenColor("3F6D2A",40) );
-
 function drawGrid(gridSize) {
   for (i = 0; i < gridSize ** 2; i++) {
-    let pixel = document.createElement("div"); // define a new div element with js called 'content'
-    pixel.classList.add("pixels"); // assign a class to our new element 'content'
+    //number of pixels needed to fill a square grid
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixels"); // assign a class to each pixel element
 
-    /* 
-        pixel.textContent = "This is the glorious text-content!"; //add some text to our new 'content' element
-        
-        pixel.setAttribute(
-          "style",
-          "width: 20px; height: 20px; background-color: hotpink;"
-        );
-*/
-
-    pixel.style.width = (1 / gridSize) * 100 + "%";
+    pixel.style.width = (1 / gridSize) * 100 + "%"; //sets the size of each pixel as a fraction of the grid size
     pixel.style.height = (1 / gridSize) * 100 + "%";
 
-    let pixelLightness = 1;
-
     pixel.addEventListener("mouseover", (event) => {
-      // highlight the mouseover target
-
       if (mouseDown == 1) {
         var pickedColor = document.getElementById("color-picker").value;
 
-        event.target.style.backgroundColor = pickedColor;
-
-        //pixelLightness = pixelLightness - 0.1;
+        event.target.style.backgroundColor = pickedColor; //colour the pixel
       }
     });
 
-    container.appendChild(pixel); //Add our new content element to the DOM
+    container.appendChild(pixel); //Add the pixel to the DOM
   }
 }
 
-drawGrid(32);
+drawGrid(32); //Draw the grid of reponsive pixels
 
 rangeInput.addEventListener("mouseup", (event) => {
+  // When you release the slider
   const pixels = document.querySelectorAll(".pixels");
 
   pixels.forEach((pixel) => {
-    pixel.remove();
+    pixel.remove(); //Erase the grid
   });
-  drawGrid(event.target.value);
+  drawGrid(event.target.value); //Redraw grid with specified size
 });
 
 let mouseDown = 0;
+
 document.body.onmousedown = function () {
   mouseDown = 1;
-  mouseInfo.textContent = "Mouse is Down!";
+  //mouseInfo.textContent = "Mouse is Down!";
 };
 document.body.onmouseup = function () {
   mouseDown = 0;
-  mouseInfo.textContent = "Mouse is Up!";
+  //mouseInfo.textContent = "Mouse is Up!";
 };
-
-/*
-function draw() {
-  if (mouseDown == 1) {
-    // the mouse is down, do what you have to do.
-    pixels.setAttribute("style", "background-color: black;");
-    
-  }
-};
-
-pixels.addEventListener("mouseover", draw());
-
-
-
-        object.addEventListener("mouseover", myScript);
-
-  pixels.addEventListener("mouseover", () => handleClick("drawing"));
-
-  function handleClick(playerSelection) {
-    playRound(playerSelection, getComputerChoice());
-  }
-*/
